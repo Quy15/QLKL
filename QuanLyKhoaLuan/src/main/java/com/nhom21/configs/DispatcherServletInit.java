4deddfe70303e2260694adc044ea4353c3eff7bf
Quy15 <hungquy10101996@gmail.com>
2023-07-25 21:36:22 +0700

    push

---
+++ QuanLyKhoaLuan/src/main/java/com/nhom21/configs/DispatcherServletInit.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nhom21.configs;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ *
+ * @author WIN10
+ */
+
+public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer{
+   
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+            WebAppContextConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{
+            "/"
+        };
+    }
+    
+}
---
+++ QuanLyKhoaLuan/src/main/java/com/nhom21/controllers/IndexController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nhom21.controllers;
+
+import javax.enterprise.inject.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author WIN10
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
